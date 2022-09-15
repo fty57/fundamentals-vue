b090edeb81c426a79365caf5b9d963fdf74f0236
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-09-15 09:41:47 -0300

    Declarações condicionais

--- crud-app/app/src/App.vue
+++ crud-app/app/src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <CustomHeader />
+  <CustomHeader v-if="showHeader" />
+
+  <div v-show="showName">Nome: {{ firstName }} Sobrenome: {{ lastName }}</div>
+
+  <div v-if="accessLevel === 'admin'">Admin</div>
+  <div v-else-if="accessLevel === 'marketing'">Marketing</div>
+  <div v-else>User</div>
+
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
@@ -13,6 +20,15 @@ export default {
   components: {
     HelloWorld,
     CustomHeader,
+  },
+  data() {
+    return {
+      showHeader: true,
+      firstName: "Álisson",
+      lastName: "Véras",
+      showName: false,
+      accessLevel: "marketing",
+    };
   },
 };
 </script>
